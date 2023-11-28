1d49f89d3e9aa6a4bd649c03026d87dc83439dea
echchabli_hamza <echchablihamza1@gmail.com>
2023-11-28 21:49:58 +0100

    container

--- lib/main.dart
+++ lib/main.dart
@@ -10,60 +10,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Clickable Buttons Example'),
+          title: Text('Container Example'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  // Handle button click
-                  print('Elevated Button Clicked');
-                },
-                child: Text('Elevated Button'),
-              ),
-              SizedBox(height: 20),
-              TextButton(
-                onPressed: () {
-                  // Handle button click
-                  print('Text Button Clicked');
-                },
-                child: Text('Text Button'),
-              ),
-              OutlinedButton(
-                onPressed: () {
-                  // Handle button click
-                  print('Outlined Button Clicked');
-                },
-                child: Text('Outlined Button'),
-              ),
-              IconButton(
-                onPressed: () {
-                  // Handle button click
-                  print('Icon Button Clicked');
-                },
-                icon: Icon(Icons.favorite),
-              ),
-              GestureDetector(
-                onTap: () {
-                  // Handle button click
-                  print('Custom Button Clicked');
-                },
-                child: Container(
-                  padding: EdgeInsets.all(12),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Text(
-                    'Custom Button',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              )
-
-            ],
+          child: Container(
+            padding :  EdgeInsets.symmetric(horizontal: 20.0  , vertical: 30.0),
+            width: 200,
+            height: 100,
+            color: Colors.blue,
+          
+            margin: EdgeInsets.all(16),
+            child: Text(
+              'Hello, Container!',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
