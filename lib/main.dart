4f337f97abc3ef6be12d4c50aab9eaa7fad20154
echchabli_hamza <echchablihamza1@gmail.com>
2023-11-28 21:14:31 +0100

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
-home :Home()
+class Home extends StatelessWidget {
+  const Home({super.key});
 
-
-));
-
-
-class Home extends StatelessWidget{
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-  appBar: AppBar(
-    title: Text('My Flutter App '),
-    centerTitle: true,
-    backgroundColor: Colors.black87,
-    ) ,
-  
-  
-  
-  body: Center(
-    child: Image (
-      image: NetworkImage('https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=459&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-      )
-
-    
-    ),
-
-    floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add your action here
-          }, 
-          child: Text('do it'),
-          backgroundColor: Colors.brown,
-
-),
-);
+      appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]),
+      body: Center(
+        child:Image(
+          image: AssetImage('c1.jpg'),
+          ) 
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+      ),
+    );
   }
 }
-
-
