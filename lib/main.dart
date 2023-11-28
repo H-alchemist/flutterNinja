293f75c3679bf671280b1abac2bce2b11ef8343c
echchabli_hamza <echchablihamza1@gmail.com>
2023-11-28 18:07:31 +0100

    image

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
 
-home :Scaffold(
+home :Home()
+
+
+));
+
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
   appBar: AppBar(
     title: Text('My Flutter App '),
     centerTitle: true,
+    backgroundColor: Colors.black87,
     ) ,
   
   
   
   body: Center(
-    child: Text("hello every one"),
+    child: Image (
+      image: NetworkImage('https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=459&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+      )
 
     
     ),
@@ -20,8 +32,12 @@ home :Scaffold(
           onPressed: () {
             // Add your action here
           }, 
-          child: Text('click'),
+          child: Text('do it'),
+          backgroundColor: Colors.brown,
 
 ),
-)));
+);
+  }
+}
+
 
